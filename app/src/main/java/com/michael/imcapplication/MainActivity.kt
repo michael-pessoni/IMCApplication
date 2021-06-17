622212af1d55ad005edd3c9e7e7d9e7052ada75b
michael-pessoni <pessoni94@gmail.com>
2021-06-17 10:22:13 -0300

    Add Listeners to make the application functional

--- app/src/main/java/com/michael/imcapplication/MainActivity.kt
+++ app/src/main/java/com/michael/imcapplication/MainActivity.kt
@@ -2,16 +2,31 @@ package com.michael.imcapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
     }
 
+    private fun setListeners() {
+        calcBTN.setOnClickListener {
+            calculate(pesoINP.text.toString(), alturaINP.text.toString())
+        }
+    }
 
+    private fun calculate(peso: String, altura: String) {
+        val peso = peso.toFloatOrNull()
+        val altura = altura.toFloatOrNull()
+
+        if (peso != null && altura != null) {
+            val imc = peso / (altura * altura)
+            titleTXT.text = "Seu IMC é \n %.2f".format(imc)
+        }
+    }
 
 
 }
